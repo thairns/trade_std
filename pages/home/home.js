a5efbe80e95a8abf7391544e084e864b137608b1
thairns <thain0419@gmail.com>
2019-01-03 16:21:18 +0800

    v1.0.0

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,77 +1,81 @@
 //home.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+//var amapFile = require('../../lib/amap-wx.js');
 
 Page({
     data: {
-    motto: 'Hello World',
-      markers: [{
-          iconPath: '/resources/others.png',
+        longi:'113.324520',
+        lati:'23.099994',
+        coverImage:'/resources/aim.svg',
+        markers: [{
+          iconPath: '/resources/target_blue.svg',
           id: 0,
           latitude: 23.099994,
           longitude: 113.324520,
-          width: 50,
-          height: 50
-      }],
-      polyline: [{
-          points: [{
-              longitude: 113.3245211,
-              latitude: 23.10229
-          }, {
-              longitude: 113.324520,
-              latitude: 23.21229
-          }],
-          color: '#FF0000DD',
-          width: 2,
-          dottedLine: true
-      }],
-      controls: [{
-          id: 1,
-          iconPath: '/resources/location.png',
-          position: {
-              left: 0,
-              top: 300 - 50,
-              width: 50,
-              height: 50
-          },
-          clickable: true
-      }],
+          width: 25,
+          height: 25
+        }],
+        wxmap:{}
     },
     //事件处理函数
     bindViewTap: function() {
     },
     onLoad: function () {
-        //获取首页信息
-        wx.request({
-            url:"https://www.thairns.com/test.php",
-            method:"post",
-            dataType:"json",
-            data:{},
+        const _this = this;
+        _this.initialize();
+        // var key = "4f2592133b9b0f2f6a19a6d7cdd457fe";
+        // var map = new amapFile.AMapWX({key:'高德Key'});
+        app.globalData.wxmap = wx.createMapContext("wxmap");
+        _this.wxmap = app.globalData.wxmap;
+        _this.wxmap.getCenterLocation({
             success:(res)=>{
-              console.log(res);
-            },
-            fail:(res)=>{
                 console.log(res);
             }
         });
+
     },
     getUserInfo: function(e) {
     },
     regionchange(e) {
-        console.log(e.type)
+        if(e.type == 'end') {
+            this.wxmap.getCenterLocation({
+                success: (res) => {
+                    this.setCentre({longitude:res.longitude,latitude:res.latitude});
+                }
+            });
+        }
     },
     markertap(e) {
-        console.log(e.markerId)
+        console.log(e);
     },
-    controltap(e) {
-        console.log(e.controlId)
-        wx.openLocation({
-            latitude: 23.362490,
-            longitude: 116.715790,
-            scale: 18,
-            name: '华乾大厦',
-            address:'金平区长平路93号'
-        })
-
+    resetCen:function(){
+        this.wxmap.moveToLocation();
+        // this.initialize();
+    },
+    initialize:function() {
+        wx.getLocation({
+            success:(res)=>{
+                var coordinate = {
+                    longitude:res.longitude,
+                    latitude:res.latitude
+                };
+                this.setScreenCentre(coordinate);
+                this.setCentre(coordinate);
+            }
+        });
+    },
+    setCentre:function (coordinate) {
+        this.setData({
+            'markers[0].latitude':coordinate.latitude,
+            'markers[0].longitude':coordinate.longitude
+        });
+    },
+    // 屏幕中心坐标设置
+    setScreenCentre:function (coordinate) {
+        this.setData({
+            longi:coordinate.longitude,
+            lati:coordinate.latitude,
+        });
     }
 })
